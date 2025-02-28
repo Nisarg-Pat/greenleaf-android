82a790fdf9a2c75f651544e85d706a217d580491
Nisarg Patel <patel321nisarg@gmail.com>
2025-02-27 19:05:03 -0500

    Added NavHostController and Topbar

--- app/src/main/java/com/infinityapps/greenleaf/GreenleafApp.kt
+++ app/src/main/java/com/infinityapps/greenleaf/GreenleafApp.kt
@@ -1,19 +1,71 @@
 package com.infinityapps.greenleaf
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.infinityapps.greenleaf.ui.theme.GreenLeafTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GreenleafAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(text = "Greenleaf")
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        }
+    )
+}
 
 @Composable
 fun GreenleafApp(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
 ) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
     GreenLeafTheme {
-        Scaffold(modifier = modifier) { innerPadding ->
+        Scaffold(
+            topBar = {
+                GreenleafAppBar(
+                    canNavigateBack = navController.previousBackStackEntry != null,
+                    navigateUp = { navController.navigateUp() }
+                )
+            },
+            modifier = modifier
+        ) { innerPadding ->
             Text(
                 text = "Android",
                 modifier = Modifier.padding(innerPadding)
@@ -22,8 +74,7 @@ fun GreenleafApp(
     }
 }
 
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreenleafAppPreview() {
     GreenleafApp()
